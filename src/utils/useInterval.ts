09fdcaba5c7d9fcbf1080c4d49fc155ce19bd4c3
Chris Chudzicki <christopher.chudzicki@gmail.com>
2024-12-10 14:53:06 -0500

    add composeRefs, useInterval

---
+++ src/utils/useInterval.ts
@@ -0,0 +1,25 @@
+import { useRef, useEffect } from "react"
+
+/**
+ * Calls a function at a specified interval.
+ *
+ * Based on https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+ */
+const useInterval = (callback: () => void, delay: number | null) => {
+  const savedCallback = useRef<() => void>()
+
+  useEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+
+  useEffect(() => {
+    if (delay !== null) {
+      const id = setInterval(() => savedCallback.current?.(), delay)
+      return () => clearInterval(id)
+    } else {
+      return () => {}
+    }
+  }, [delay])
+}
+
+export { useInterval }
