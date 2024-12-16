a3924d22e83edbb40eca0b9153336edd02a926dd
Chris Chudzicki <christopher.chudzicki@gmail.com>
2024-12-16 13:08:56 -0500

    better comment

--- src/utils/useDevCheckStable.ts
+++ src/utils/useDevCheckStable.ts
@@ -1,15 +1,22 @@
 import { useEffect, useRef } from "react"
 
+/**
+ * Emits `console.error` if two subsequent values of `jsonSerializable` serialize
+ * to the same thing but are different references.
+ *
+ * This hook does NOT run in production.
+ */
 const useDevCheckStable = (
   jsonSerializable: unknown,
   msg = "The value has changed. This may cause unnecessary re-renders",
 ) => {
-  const valRef = useRef(jsonSerializable)
   if (process.env.NODE_ENV !== "production") {
     /**
      * Calling hooks conditionally based on env vars is not really a problem.
      * In a given environment, the hook will always run or always not run.
      */
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const valRef = useRef(jsonSerializable)
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useEffect(() => {
       const sameJson =
