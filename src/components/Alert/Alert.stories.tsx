fe0172b6a1233d7b357ebef5cfbba7648cd34299
Chris Chudzicki <christopher.chudzicki@gmail.com>
2025-02-19 15:14:15 -0500

    move alert

---
+++ src/components/Alert/Alert.stories.tsx
@@ -0,0 +1,75 @@
+import * as React from "react"
+import type { Meta, StoryObj } from "@storybook/react"
+import { Alert } from "./Alert"
+import Stack from "@mui/material/Stack"
+
+const meta: Meta<typeof Alert> = {
+  title: "smoot-design/Alert",
+  component: Alert,
+}
+
+export default meta
+
+type Story = StoryObj<typeof Alert>
+
+export const Basic: Story = {
+  args: {
+    severity: "info",
+  },
+  render: (args) => (
+    <Alert {...args}>Alert with severity "{args.severity}"</Alert>
+  ),
+}
+
+export const Closable: Story = {
+  args: {
+    severity: "warning",
+    closable: true,
+  },
+  render: (args) => (
+    <Alert {...args}>Closable alert with severity "{args.severity}"</Alert>
+  ),
+}
+
+export const Variants: Story = {
+  argTypes: {
+    severity: {
+      table: {
+        disable: true,
+      },
+    },
+    closable: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+  render: (args) => (
+    <Stack direction="column" gap={2} sx={{ my: 2 }}>
+      <Alert {...args} severity="info">
+        Alert with severity "info"
+      </Alert>
+      <Alert {...args} closable severity="info">
+        Closable alert with severity "info"
+      </Alert>
+      <Alert {...args} severity="success">
+        Alert with severity "success"
+      </Alert>
+      <Alert {...args} closable severity="success">
+        Closable alert with severity "success"
+      </Alert>
+      <Alert {...args} severity="warning">
+        Alert with severity "warning"
+      </Alert>
+      <Alert {...args} closable severity="warning">
+        Closable alert with severity "warning"
+      </Alert>
+      <Alert {...args} severity="error">
+        Alert with severity "error"
+      </Alert>
+      <Alert {...args} closable severity="error">
+        Closable alert with severity "error"
+      </Alert>
+    </Stack>
+  ),
+}
---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,106 @@
+"use client"
+
+import * as React from "react"
+
+import styled from "@emotion/styled"
+import { default as MuiAlert } from "@mui/material/Alert"
+import type { AlertColor } from "@mui/material/Alert"
+import { Theme } from "@emotion/react"
+
+const getColor = (theme: Theme, severity: AlertColor) => {
+  return {
+    info: theme.custom.colors.blue,
+    success: theme.custom.colors.green,
+    warning: theme.custom.colors.orange,
+    error: theme.custom.colors.lightRed,
+  }[severity]
+}
+
+type AlertStyleProps = {
+  severity: AlertColor
+}
+
+const AlertStyled = styled(MuiAlert)<AlertStyleProps>(
+  ({ theme, severity }) => ({
+    padding: "11px 16px",
+    borderRadius: 4,
+    borderWidth: 2,
+    borderStyle: "solid",
+    borderColor: getColor(theme, severity),
+    background: "#FFF",
+    ".MuiAlert-message": {
+      ...theme.typography.body2,
+      color: theme.custom.colors.darkGray2,
+      alignSelf: "center",
+    },
+    "> div": {
+      paddingTop: 0,
+      paddingBottom: 0,
+    },
+    ".MuiAlert-icon": {
+      marginRight: 8,
+      svg: {
+        width: 16,
+        fill: getColor(theme, severity),
+      },
+    },
+    button: {
+      padding: 0,
+      ":hover": {
+        margin: 0,
+        background: "none",
+      },
+    },
+  }),
+)
+
+const Hidden = styled.span({ display: "none" })
+
+type AlertProps = {
+  visible?: boolean
+  closable?: boolean
+  className?: string
+  severity?: AlertColor
+  /**
+   * Alert Content
+   */
+  children?: React.ReactNode
+}
+
+const Alert: React.FC<AlertProps> = ({
+  visible = true,
+  severity = "info",
+  closable,
+  children,
+  className,
+}) => {
+  const [_visible, setVisible] = React.useState(visible)
+  const id = React.useId()
+  const onCloseClick = () => {
+    setVisible(false)
+  }
+
+  React.useEffect(() => {
+    setVisible(visible)
+  }, [visible])
+
+  if (!_visible) {
+    return null
+  }
+
+  return (
+    <AlertStyled
+      severity={severity!}
+      onClose={closable ? onCloseClick : undefined}
+      role="alert"
+      aria-describedby={id}
+      className={className}
+    >
+      {children}
+      <Hidden id={id}>{severity} message</Hidden>
+    </AlertStyled>
+  )
+}
+
+export { Alert }
+export type { AlertProps }
