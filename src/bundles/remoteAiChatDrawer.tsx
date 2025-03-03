8469de534389bddb98bde95eaacc885cc3feda16
Chris Chudzicki <christopher.chudzicki@gmail.com>
2025-03-03 13:55:15 -0500

    render drawer in shadow dom to isolate styles

--- src/bundles/remoteAiChatDrawer.tsx
+++ src/bundles/remoteAiChatDrawer.tsx
@@ -2,16 +2,45 @@ import * as React from "react"
 import { createRoot } from "react-dom/client"
 import { AiChatDrawer } from "./RemoteAiChatDrawer/RemoteAiChatDrawer"
 import type { AiChatDrawerProps } from "./RemoteAiChatDrawer/RemoteAiChatDrawer"
-import { ThemeProvider } from "../components/ThemeProvider/ThemeProvider"
+import {
+  ThemeProvider,
+  createTheme,
+} from "../components/ThemeProvider/ThemeProvider"
+import { CacheProvider } from "@emotion/react"
+import createCache from "@emotion/cache"
 
+/**
+ * Renders the AiChatDrawer in an shadow DOM in order to isolate the drawer
+ * styles from external stylesheets.
+ */
 const init = (opts: AiChatDrawerProps) => {
-  const root = document.createElement("div")
-  root.id = "smoot-chat-drawer-root"
-  document.body.append(root)
-  createRoot(root).render(
-    <ThemeProvider>
-      <AiChatDrawer {...opts} />
-    </ThemeProvider>,
+  const container = document.createElement("div")
+  document.body.appendChild(container)
+  const shadowContainer = container.attachShadow({ mode: "open" })
+  const shadowRootEl = document.createElement("div")
+  shadowRootEl.id = "smoot-chat-drawer-root"
+  shadowContainer.append(shadowRootEl)
+  // See https://mui.com/material-ui/customization/shadow-dom/
+  // Ensure style tags are rendered in shadow root
+  const cache = createCache({
+    key: "css",
+    prepend: true,
+    container: shadowContainer,
+  })
+  const theme = createTheme({
+    components: {
+      // Ensure modals, etc, are rendered in shadow root
+      MuiPopover: { defaultProps: { container: shadowRootEl } },
+      MuiPopper: { defaultProps: { container: shadowRootEl } },
+      MuiModal: { defaultProps: { container: shadowRootEl } },
+    },
+  })
+  createRoot(shadowRootEl).render(
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>
+        <AiChatDrawer {...opts} />
+      </ThemeProvider>
+    </CacheProvider>,
   )
 }
 
