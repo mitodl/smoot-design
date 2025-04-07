e527e828e7c9a9f041948bfe28753b0065e43c72
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2025-04-07 21:32:04 +0100

    Format

--- jest.config.ts
+++ jest.config.ts
@@ -12,9 +12,7 @@ const config: Config.InitialOptions = {
   transform: {
     "^.+\\.(t|j)sx?$": "@swc/jest",
   },
-  transformIgnorePatterns: [
-    "node_modules/"
-  ],
+  transformIgnorePatterns: ["node_modules/"],
   moduleNameMapper: {
     "\\.(css|scss|svg|jpg|jpeg|png)$": "<rootDir>/test-utils/filemock.js",
     "^rehype-raw$": "<rootDir>/test-utils/modulemock.js",
