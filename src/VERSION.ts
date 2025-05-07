96fb373b6e01e97d9b530ead75bc3f706cfccb40
Chris Chudzicki <christopher.chudzicki@gmail.com>
2025-05-07 09:10:16 -0400

    expand comment

--- src/VERSION.ts
+++ src/VERSION.ts
@@ -2,5 +2,11 @@
  * This file is auto-generated at build time.
  * Run node ./scripts/set_version.js to update the version.
  * Do not update this file manually.
+ *
+ * NOTES:
+ * - In development, VERSION will always be "0.0.0"
+ * - The version should not simply be imported from package.json. This would
+ *   result in all of the package.json being included in the bundled code, which
+ *   is not desired.
  */
 export const VERSION = "0.0.0"
