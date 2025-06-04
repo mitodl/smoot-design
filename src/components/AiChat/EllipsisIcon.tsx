fae4fd9ab4eafe57c39a4ca4d0710e90705daac4
Jon Kafton <939376+jonkafton@users.noreply.github.com>
2025-06-04 16:46:55 +0200

    Add to tsx

--- src/components/AiChat/EllipsisIcon.tsx
+++ src/components/AiChat/EllipsisIcon.tsx
@@ -15,7 +15,7 @@ const EllipsisIcon: FC<EllipsisIconProps> = ({ className }) => (
       <animate
         attributeName="cy"
         values="15;4;15;15"
-        dur="2s"
+        dur="1.2s"
         keyTimes="0;0.3;0.6;1"
         keySplines="0.42 0 0.58 1; 0.42 0 0.58 1; 0.42 0 0.58 1"
         calcMode="spline"
@@ -24,8 +24,8 @@ const EllipsisIcon: FC<EllipsisIconProps> = ({ className }) => (
       ></animate>
       <animate
         attributeName="ry"
-        values="8;8;6;8;8"
-        dur="2s"
+        values="8;8;7;8;8"
+        dur="1.2s"
         keyTimes="0;0.52;0.6;0.7;1"
         repeatCount="indefinite"
         begin="0s"
@@ -33,7 +33,7 @@ const EllipsisIcon: FC<EllipsisIconProps> = ({ className }) => (
       <animate
         attributeName="rx"
         values="8;8;9;8;8"
-        dur="2s"
+        dur="1.2s"
         keyTimes="0;0.52;0.6;0.7;1"
         repeatCount="indefinite"
         begin="0s"
@@ -43,56 +43,56 @@ const EllipsisIcon: FC<EllipsisIconProps> = ({ className }) => (
       <animate
         attributeName="cy"
         values="15;4;15;15"
-        dur="2s"
+        dur="1.2s"
         keyTimes="0;0.3;0.6;1"
         keySplines="0.42 0 0.58 1; 0.42 0 0.58 1; 0.42 0 0.58 1"
         calcMode="spline"
         repeatCount="indefinite"
-        begin="0.3s"
+        begin="0.2s"
       ></animate>
       <animate
         attributeName="ry"
-        values="8;8;6;8;8"
-        dur="2s"
+        values="8;8;7;8;8"
+        dur="1.2s"
         keyTimes="0;0.52;0.6;0.7;1"
         repeatCount="indefinite"
-        begin="0.3s"
+        begin="0.2s"
       ></animate>
       <animate
         attributeName="rx"
         values="8;8;9;8;8"
-        dur="2s"
+        dur="1.2s"
         keyTimes="0;0.52;0.6;0.7;1"
         repeatCount="indefinite"
-        begin="0.3s"
+        begin="0.2s"
       ></animate>
     </ellipse>
     <ellipse cx="80" cy="15" rx="8" ry="8" fill="#000">
       <animate
         attributeName="cy"
         values="15;4;15;15"
-        dur="2s"
+        dur="1.2s"
         keyTimes="0;0.3;0.6;1"
         keySplines="0.42 0 0.58 1; 0.42 0 0.58 1; 0.42 0 0.58 1"
         calcMode="spline"
         repeatCount="indefinite"
-        begin="0.6s"
+        begin="0.4s"
       ></animate>
       <animate
         attributeName="ry"
-        values="8;8;6;8;8"
-        dur="2s"
+        values="8;8;7;8;8"
+        dur="1.2s"
         keyTimes="0;0.52;0.6;0.7;1"
         repeatCount="indefinite"
-        begin="0.6s"
+        begin="0.4s"
       ></animate>
       <animate
         attributeName="rx"
         values="8;8;9;8;8"
-        dur="2s"
+        dur="1.2s"
         keyTimes="0;0.52;0.6;0.7;1"
         repeatCount="indefinite"
-        begin="0.6s"
+        begin="0.4s"
       ></animate>
     </ellipse>
   </svg>
