a7a509b1f27b46f01cf572e76aa67eb1c21eebfc
Chris Chudzicki <christopher.chudzicki@gmail.com>
2025-06-13 12:19:08 -0400

    fix a default import

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -1,4 +1,5 @@
-import React, { useState, ChangeEvent } from "react"
+import * as React from "react"
+import { useState, ChangeEvent } from "react"
 import type { Meta, StoryObj } from "@storybook/react"
 import { Checkbox } from "./Checkbox"
 import type { CheckboxProps } from "./Checkbox"
